c2f28d804a3763e61ea907c613ed54aee8cb3e1d
maryLaptop <marylein90@gmail.com>
2015-10-31 17:26:23 +0100

    finished implementation of imageviewer

--- src/projects/ImageViewer.java
+++ src/projects/ImageViewer.java
@@ -13,44 +13,63 @@ import javafx.stage.FileChooser.ExtensionFilter;
 import java.io.File;
 
 /**
+ * This class is an javafx-application to show a chosen image.
+ *
  * @author Marie Hennings
  */
 public class ImageViewer extends Application {
 
+    /**
+     * Array of possible image file extensions
+     */
+    private final String[] filesExtensions = new String[]{"*.bmp", "*.gif", "*.jpg", "*.jpeg", "*.png"};
+
+    /**
+     * Main method calling javafx-application main
+     *
+     * @param args Starting arguments
+     */
     public static void main(String[] args) {
         launch(args);
     }
 
+    /**
+     * Method opens a filechooser and shows the chosen image in a window with the images size
+     *
+     * @param primaryStage Main Stage of the program
+     */
     public void start(final Stage primaryStage) {
         try {
-            Scene scene = showImage(chooseImage(primaryStage));
+            //choose file with an image extension
+            final FileChooser fileChooser = new FileChooser();
+            fileChooser.getExtensionFilters().add(new ExtensionFilter("Images", filesExtensions));
+            final File imageFile = fileChooser.showOpenDialog(primaryStage);
+
+            //if a file has been chosen load image, close otherwise
+            Image image;
+            if (imageFile != null) {
+                image = new Image("file:" + imageFile.getAbsolutePath());
+            } else {
+                //primaryStages needs to be shown before closing is possible
+                primaryStage.show();
+                primaryStage.close();
+                return;
+            }
+
+            // show image in ImageView and put it in a pane and on a scene with the same size
+            final ImageView imgViewer = new ImageView(image);
+            final Pane pane = new Pane();
+            pane.getChildren().add(imgViewer);
+            final Scene scene = new Scene(pane, image.getWidth(), image.getHeight());
+
+            //set scene, title and avoid resizability
             primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+            primaryStage.setTitle(imageFile.getName());
+
             primaryStage.show();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-    private Image chooseImage(final Stage primaryStage) {
-        FileChooser fileChooser = new FileChooser();
-        //TODO Extensionfilter
-        //fileChooser.getExtensionFilters().addAll(new ExtensionFilter("Bild Dateien", "*.jpg", "*.png", "*.bmp", "*.gif", "*.jpeg"));
-
-        File imageFile = fileChooser.showOpenDialog(primaryStage);
-
-        if(imageFile != null) {
-            return new Image("file:" + File.separator + File.separator + File.separator + imageFile.getPath());
-        }
-        return null;
-    }
-
-    private Scene showImage(final Image image) {
-        double height = image.getHeight();
-        double width = image.getWidth();
-        ImageView imgViewer = new ImageView(image);
-        Pane pane = new Pane();
-        pane.getChildren().add(imgViewer);
-        return new Scene(pane, width, height);
-    }
-
 }
