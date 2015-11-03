4f9755f3aca968159f81dc5b116d5c23a2790857
Steven <kreven@web.de>
2015-11-03 16:33:12 +0100

    added a menubar with a savefunction in png and jpg

--- src/projects/ImageSaver.java
+++ src/projects/ImageSaver.java
@@ -5,13 +5,21 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class ImageSaver extends Application {
 
@@ -28,11 +36,21 @@ public class ImageSaver extends Application {
         try {
             refreshImage();
 
-            final Pane pane = new Pane();
-            pane.getChildren().add(view);
+            final BorderPane pane = new BorderPane();
+            pane.setCenter(view);
             final Scene scene = new Scene(pane, width, height);
             stage.setScene(scene);
             stage.setTitle("Image Saver");
+
+            MenuBar menubar = new MenuBar();
+            Menu filemenu = new Menu("File");
+            MenuItem save = new MenuItem("Save...");
+            save.setOnAction(e -> saveImage(stage));
+
+            filemenu.getItems().add(save);
+            menubar.getMenus().add(filemenu);
+
+            pane.setTop(menubar);
             stage.show();
 
             scene.widthProperty().addListener(new ChangeListener<Number>() {
@@ -60,6 +78,27 @@ public class ImageSaver extends Application {
     private void refreshImage() {
         Image image = SwingFXUtils.toFXImage(getThinRedLine(width, height), null);
         view.setImage(image);
+    }
+
+    private void saveImage(Stage stage){
+        FileChooser fileDialog = new FileChooser();
+        fileDialog.setTitle("Save");
+        fileDialog.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("PNG Image", "*.png"),
+                new FileChooser.ExtensionFilter("JPG Image", "*.jpg"));
+
+        File file = fileDialog.showSaveDialog(stage);
+
+        if(file != null){
+            String fileExt = fileDialog.getSelectedExtensionFilter().getExtensions().get(0);
+
+            try {
+                ImageIO.write(getThinRedLine(width,height),fileExt.split("\\.")[1], file);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
     private BufferedImage getThinRedLine(final int width, final int height) {
