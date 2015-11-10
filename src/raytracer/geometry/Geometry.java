01d7d380fb9e2de5a433d4b2e158355f65fa01c5
Steven <kreven@web.de>
2015-11-10 09:33:16 +0100

    geometry-class added

---
+++ src/raytracer/geometry/Geometry.java
@@ -0,0 +1,38 @@
+package raytracer.geometry;
+
+/**
+ * Created by Steve-O on 10.11.2015.
+ */
+public abstract class Geometry {
+
+    Color color;
+
+    public Geometry(Color color){
+        this.color = color;
+    }
+
+    public abstract Hit hit(Ray r);
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Geometry geometry = (Geometry) o;
+
+        return !(color != null ? !color.equals(geometry.color) : geometry.color != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return color != null ? color.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Geometry{" +
+                "color=" + color +
+                '}';
+    }
+}
