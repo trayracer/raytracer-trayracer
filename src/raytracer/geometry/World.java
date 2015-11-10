c0d588cc274947e8b70bd159dbd985434fdcf681
Oliver Kniejski <o.kniejski@gmail.com>
2015-11-10 14:44:20 +0100

    implemented world

--- src/raytracer/geometry/World.java
+++ src/raytracer/geometry/World.java
@@ -1,7 +1,49 @@
 package raytracer.geometry;
 
+import raytracer.math.Ray;
+import raytracer.texture.Color;
+
+import java.util.LinkedList;
+import java.util.List;
+
 /**
- * Created by ok on 08.11.15.
+ * This class
+ *
+ * @author Oliver Kniejski
  */
 public class World {
+
+    public final Color backgroundColor;
+    private List<Geometry> objects = new LinkedList<>();
+
+    public World(Color backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public Hit hit(Ray ray) {
+        if (objects.size() == 0) return null;
+        List<Hit> hits = new LinkedList<>();
+
+        for (Geometry geo : objects) {
+            if (geo.hit(ray) != null) {
+                hits.add(geo.hit(ray));
+            }
+        }
+        if (hits.size() == 0) return null;
+
+        Hit smallestHit = hits.get(0);
+
+        if (hits.size()>1) {
+            for (int i = 1; i < hits.size(); i++) {
+                if (hits.get(i).t < smallestHit.t) {
+                    smallestHit = hits.get(i);
+                }
+            }
+        }
+        return smallestHit;
+    }
+
+    public void addGeometry(Geometry geo) {
+        this.objects.add(geo);
+    }
 }
