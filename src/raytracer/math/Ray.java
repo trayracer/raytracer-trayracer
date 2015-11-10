b912bd862e20bf2b609001925b67a23e3cf78703
Steven <kreven@web.de>
2015-11-10 10:48:05 +0100

    ray-class finished, but not commented

--- src/raytracer/math/Ray.java
+++ src/raytracer/math/Ray.java
@@ -4,21 +4,48 @@ package raytracer.math;
  * Created by Steve-O on 10.11.2015.
  */
 public class Ray {
-   final Point3 o;
-   final Vector3 d;
+   public final Point3 o;
+   public final Vector3 d;
 
-    public Ray(Point3 o, Vector3 d) {
+    public Ray(final Point3 o, final Vector3 d) {
         this.o = o;
         this.d = d;
     }
 
-    public Point3 at(double t){
-
-        return new Point3(t*o.x,t*o.y,t*o.z);
+    public final Point3 at(final double t){
+        Point3 at = o.add(d.mul(t));
+        return at;
     }
 
-    public double tOf(Point3 p){
+    public final double tOf(final Point3 p){
+        final double t = (o.sub(p).magnitude / d.magnitude);
+        return t;
+    }
 
-        return new double((p.sub(o).magnitude/d.magnitude));
+    @Override
+    public boolean equals(Object o1) {
+        if (this == o1) return true;
+        if (o1 == null || getClass() != o1.getClass()) return false;
+
+        Ray ray = (Ray) o1;
+
+        if (!o.equals(ray.o)) return false;
+        return d.equals(ray.d);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = o.hashCode();
+        result = 31 * result + d.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Ray{" +
+                "o=" + o +
+                ", d=" + d +
+                '}';
     }
 }
