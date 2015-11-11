a8d1875c4ea939f1cb90ae1cc43db47c386441de
maryLaptop <marylein90@gmail.com>
2015-11-11 10:18:09 +0100

    started implementation of raytracer (color left), template for report

---
+++ src/raytracer/ui/Raytracer.java
@@ -0,0 +1,153 @@
+package raytracer.ui;/**
+ * Created on 10.11.2015.
+ *
+ * @author Marie Hennings
+ */
+
+import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import raytracer.camera.Camera;
+import raytracer.camera.OrthographicCamera;
+import raytracer.camera.PerspectiveCamera;
+import raytracer.geometry.*;
+import raytracer.math.Normal3;
+import raytracer.math.Point3;
+import raytracer.math.Vector3;
+import raytracer.texture.Color;
+
+import java.awt.image.BufferedImage;
+
+public class Raytracer extends Application {
+    /**
+     * Width of the image and window content
+     */
+    private int width = 640;
+    /**
+     * Height of the image and window content
+     */
+    private int height = 480;
+    /**
+     * World which will be shown
+     */
+    World world;
+    /**
+     * Camera that is used, perspective or orthographic
+     */
+    Camera cam;
+
+    /**
+     * Main method calling javafx-application main
+     *
+     * @param args Starting arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(final Stage primaryStage) {
+        final Pane pane = new Pane();
+
+        //planeScene();
+        //sphereScene();
+        //boxScene();
+        triangleScene();
+        //twoSpheresPerspective();
+        //twoSpheresOrthographic();
+
+        Image image = SwingFXUtils.toFXImage(drawWorld(), null);
+        ImageView view = new ImageView();
+        view.setImage(image);
+        pane.getChildren().add(view);
+
+        final Scene scene = new Scene(pane, width, height);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * This method draws the world for the perspective of the given camera and saves it in a bufferedimage.
+     * @return The BufferedImage the world is drawn in.
+     */
+    private BufferedImage drawWorld() {
+        BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                Hit hit = world.hit(cam.rayFor(width, height, x, y));
+
+                //TODO Color
+                if (hit != null) {
+                    java.awt.Color color = new java.awt.Color((int)hit.geo.color.r*255,(int)hit.geo.color.g*255,(int)hit.geo.color.b*255);
+                    bufferedImage.setRGB(x, y, color.getRGB());
+                } else {
+                    bufferedImage.setRGB(x, y, 0);
+                }
+            }
+        }
+        return bufferedImage;
+    }
+
+    /**
+     * Sets a perspective camera and adds a plane to the world.
+     */
+    private void planeScene() {
+        cam = new PerspectiveCamera(new Point3(0, 0, 0), new Vector3(0, 0, -1), new Vector3(0, 1, 0), Math.PI / 4);
+        world = new World(new Color(0, 0, 0));
+        world.addGeometry(new Plane(new Point3(0, -1, 0), new Normal3(0, 1, 0), new Color(0, 1, 0)));
+        ;
+    }
+
+    /**
+     * Sets a perspective camera and adds a sphere to the world.
+     */
+    private void sphereScene() {
+        cam = new PerspectiveCamera(new Point3(0, 0, 0), new Vector3(0, 0, -1), new Vector3(0, 1, 0), Math.PI / 4);
+        world = new World(new Color(0, 0, 0));
+        world.addGeometry(new Sphere(new Point3(0, 0, -1), 0.5, new Color(1, 0, 0)));
+    }
+
+    /**
+     * Sets a perspective camera and adds a box to the world.
+     */
+    private void boxScene() {
+        cam = new PerspectiveCamera(new Point3(3, 3, 3), new Vector3(-3, -3, -3), new Vector3(0, 1, 0), Math.PI / 4);
+        world = new World(new Color(0, 0, 0));
+        //TODO AxisAlignedBox
+        //world.addGeometry();
+    }
+
+    /**
+     * Sets a perspective camera and adds a triangle to the world.
+     */
+    private void triangleScene() {
+        cam = new PerspectiveCamera(new Point3(0, 0, 0), new Vector3(0, 0, -1), new Vector3(0, 1, 0), Math.PI / 4);
+        world = new World(new Color(0, 0, 0));
+        world.addGeometry(new Triangle(new Point3(-0.5, 0.5, -3), new Point3(0.5, 0.5, -3), new Point3(0.5, -0.5, -3), new Color(1, 0, 1)));
+    }
+
+    /**
+     * Sets a perspective scamera and adds two spheres to the world.
+     */
+    private void twoSpheresPerspective() {
+        cam = new PerspectiveCamera(new Point3(0, 0, 0), new Vector3(0, 0, -1), new Vector3(0, 1, 0), Math.PI / 4);
+        world = new World(new Color(0, 0, 0));
+        world.addGeometry(new Sphere(new Point3(-1, 0, -3), 0.5, new Color(1, 0, 0)));
+        world.addGeometry(new Sphere(new Point3(1, 0, -6), 0.5, new Color(1, 0, 0)));
+    }
+
+    /**
+     * Sets a orthographic camera and adds two spheres to the world.
+     */
+    private void twoSpheresOrthographic() {
+        cam = new OrthographicCamera(new Point3(0, 0, 0), new Vector3(0, 0, -1), new Vector3(0, 1, 0), 3);
+        world = new World(new Color(0, 0, 0));
+        world.addGeometry(new Sphere(new Point3(-1, 0, -3), 0.5, new Color(1, 0, 0)));
+        world.addGeometry(new Sphere(new Point3(1, 0, -6), 0.5, new Color(1, 0, 0)));
+    }
+}
