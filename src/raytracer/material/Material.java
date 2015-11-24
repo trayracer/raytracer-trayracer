e5411dba87e6c46f88785bfadb94a1dc66bf9fc6
maryLaptop <marylein90@gmail.com>
2015-11-24 08:41:40 +0100

    bugfix in material

--- src/raytracer/material/Material.java
+++ src/raytracer/material/Material.java
@@ -2,19 +2,21 @@ package raytracer.material;
 
 import raytracer.geometry.Hit;
 import raytracer.geometry.World;
+import raytracer.texture.Color;
 
 /**
+ * An abstract class which represents a material.
  *
- * 
  * @author Marie Hennings
  */
 public abstract class Material {
 
     /**
+     * This method
      *
-     * @param hit
-     * @param world
-     * @return
+     * @param hit The hit.
+     * @param world The world with the lights.
+     * @return Color of the Point of the Hit.
      */
-    public abstract World colorFor(Hit hit, World world);
+    public abstract Color colorFor(final Hit hit, final World world);
 }
--- src/raytracer/material/SingleColorMaterial.java
+++ src/raytracer/material/SingleColorMaterial.java
@@ -1,7 +1,22 @@
 package raytracer.material;
 
+import raytracer.geometry.Hit;
+import raytracer.geometry.World;
+import raytracer.texture.Color;
+
 /**
  * Created on 24.11.15.
  */
-public class SingleColorMaterial {
+public class SingleColorMaterial extends Material {
+
+    public final Color color;
+
+    public SingleColorMaterial(final Color color){
+        this.color = color;
+    }
+
+    @Override
+    public Color colorFor(final Hit hit, final World world) {
+        return null;
+    }
 }
