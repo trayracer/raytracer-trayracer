1723d6920f3aa8f3c5eac4c09ee2abab64614f5b
maryLaptop <marylein90@gmail.com>
2015-12-08 09:59:17 +0100

    scenes for exercise 4

--- src/raytracer/material/ReflectiveMaterial.java
+++ src/raytracer/material/ReflectiveMaterial.java
@@ -1,7 +1,28 @@
 package raytracer.material;
 
+import raytracer.geometry.Hit;
+import raytracer.geometry.World;
+import raytracer.texture.Color;
+
 /**
  * Created by ok on 08.12.15.
  */
-public class ReflectiveMaterial {
+public class ReflectiveMaterial extends Material{
+
+    public final Color diffuse;
+    public final Color specular;
+    public final int exponent;
+    public final Color reflection;
+
+    public ReflectiveMaterial(final Color diffuse, final Color specular, final int exponent, final Color reflection){
+        this.diffuse = diffuse;
+        this.specular = specular;
+        this.exponent = exponent;
+        this.reflection = reflection;
+    }
+
+    @Override
+    public Color colorFor(final Hit hit, final World world, final Tracer tracer) {
+        return null;
+    }
 }
