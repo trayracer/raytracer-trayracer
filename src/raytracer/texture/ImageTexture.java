65a69dbf833df795c8fa496ee8f377e91bd3189f
maryLaptop <marylein90@gmail.com>
2015-12-16 16:04:15 +0100

    fixed color in ImageTexture

--- src/raytracer/texture/ImageTexture.java
+++ src/raytracer/texture/ImageTexture.java
@@ -27,10 +27,10 @@ public class ImageTexture implements Texture {
         double u = coord2.u;
         double v = coord2.v;
 
-        if (u > 1.0){
+        if (u > 1.0) {
             u = u % 1.0;
         }
-        if (v > 1.0){
+        if (v > 1.0) {
             v = v % 1.0;
         }
         if (u < 0.0) {
@@ -44,25 +44,10 @@ public class ImageTexture implements Texture {
         int y = (int) ((image.getHeight() - 1) - (image.getHeight() - 1) * v);
 
         Color color;
-//        if (image.getPixelReader() == null) {
-//            return new Color(0, 1, 0);
-//        }
-//        if (y > image.getHeight() || y < 0 || x > image.getWidth() || x < 0){
-//            System.out.println("h: " + image.getHeight() + " w: " + image.getWidth() + " u: " + u + ", v: " + v + " x: " + x + ", y: " + y ); // " color: " + image.getPixelReader().getArgb(x, y));
-//        }
-//        if (image.getPixelReader().getColor(x, y) != null) {
-//            javafx.scene.paint.Color colorFX = image.getPixelReader().getColor(x, y);
-//            color = new Color(colorFX.getRed() / 255, colorFX.getGreen() / 255, colorFX.getBlue() / 255);
-//        } else {
-//            System.out.println("Color is null");
-//            color = new Color(1, 0, 0);
-//        }
-        int argb = image.getPixelReader().getArgb(x, y);
-        int r = (argb & 0xff0000) >> 16;
-        int g = (argb & 0xff00) >> 8;
-        int b = (argb & 0xff);
-        color = new Color(r / 255.0, g / 255.0, b / 255.0);
 
+        javafx.scene.paint.Color colorFX = image.getPixelReader().getColor(x, y);
+        color = new Color(colorFX.getRed(), colorFX.getGreen(), colorFX.getBlue());
+        
         return color;
     }
 }
